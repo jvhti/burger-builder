bd0fbf849e39a04eea9c138334830ccf69aa3234
jvhti <jvhti@hotmail.com>
2020-09-25 21:58:07 -0300

    Show message if ingredients list is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,14 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.scss';
 
 const burger = ({ingredients}) => {
-  const transformedIngredients = Object.keys(ingredients).map((key) => {
+  let transformedIngredients = Object.keys(ingredients).map((key) => {
     return [...Array(ingredients[key])].map((_, i) =>
         <BurgerIngredient key={key + i} type={key}/>
     );
-  });
+  }).reduce((arr, el) => arr.concat(el), []);
+
+  if (transformedIngredients.length === 0)
+    transformedIngredients = <p>Please start adding ingredients</p>;
 
   return (
       <div className={classes.Burger}>
