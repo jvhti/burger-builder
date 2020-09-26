c1acd24f2678e03d69e2386c3dbbf1d3d8ffe55b
jvhti <jvhti@hotmail.com>
2020-09-25 23:38:26 -0300

    Create Backdrop component; Use Backdrop on Modal;

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from './Modal.module.scss';
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => {
   return (
-      <div className={classes.Modal}
-           style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
-        {props.children}
-      </div>
+      <React.Fragment>
+        <div className={classes.Modal}
+             style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
+          {props.children}
+        </div>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
+      </React.Fragment>
   );
 };
 
