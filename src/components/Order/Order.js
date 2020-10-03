634bbaebe6d5e204b3604725c1e22954b14b6d73
jvhti <jvhti@hotmail.com>
2020-10-03 19:52:39 -0300

    Add key to ingredients list in Order component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,13 +5,20 @@ const order = (props) => {
   const ingredients = [];
 
   for (const ingredientName in props.ingredients)
-    ingredients.push(props.ingredients[ingredientName] + ' x ' + ingredientName.charAt(0).toUpperCase() + ingredientName.slice(1));
+    ingredients.push({
+      name: ingredientName,
+      text: props.ingredients[ingredientName] + ' x ' + ingredientName.charAt(0).toUpperCase() + ingredientName.slice(1)
+    });
 
 
   return (
       <div className={classes.Order}>
-        <p>Ingredients: {ingredients.map(x => <span
-            style={{border: '1px solid #ccc', marginRight: '5px', padding: '2px 4px'}}>{x}</span>)} </p>
+        <p>Ingredients: {ingredients.map(x => <span key={x.name}
+                                                    style={{
+                                                      border: '1px solid #ccc',
+                                                      marginRight: '5px',
+                                                      padding: '2px 4px'
+                                                    }}>{x.text}</span>)} </p>
         <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
       </div>
   );
