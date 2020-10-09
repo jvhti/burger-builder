66b081ecf713eb7501bc8f030011c5fccb9cb7a2
jvhti <jvhti@hotmail.com>
2020-10-09 18:25:32 -0300

    Guard routes based on if the user is authenticated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import {Route, Switch, withRouter} from "react-router-dom";
+import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
@@ -15,22 +15,39 @@ class App extends Component {
   }
 
   render() {
-    return (
-        <Layout>
+    let routes = (
+        <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to="/"/>
+        </Switch>
+    );
+
+    if (this.props.isAuthenticated)
+      routes = (
           <Switch>
             <Route path="/checkout" component={Checkout}/>
             <Route path="/orders" component={Orders}/>
-            <Route path="/auth" component={Auth}/>
             <Route path="/logout" component={Logout}/>
-            <Route path="/" component={BurgerBuilder}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+            <Redirect to="/"/>
           </Switch>
+      );
+
+    return (
+        <Layout>
+          {routes}
         </Layout>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.token !== null
+});
+
 const mapDispatchToProps = dispatch => ({
   onTryAutoSignUp: () => dispatch(actions.authCheckState())
 })
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
