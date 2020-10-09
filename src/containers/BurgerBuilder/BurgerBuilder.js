b07a547ca8caaedb1eb4932e4c9a17ee8ada2cf9
jvhti <jvhti@hotmail.com>
2020-10-09 20:15:52 -0300

    Write test for BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Loader from "../../hoc/Loader/Loader";
 import {connect} from "react-redux";
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   }
