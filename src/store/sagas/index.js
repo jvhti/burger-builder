58757cedd34460b327db026d163da70945f34255
jvhti <jvhti@hotmail.com>
2020-10-10 12:56:57 -0300

    Create and use Init Ingredients Saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,22 @@
 import {takeEvery} from 'redux-saga/effects';
 
 import {authCheckStateSaga, authUserSaga, checkAuthTimeoutSaga, logoutSaga} from './auth';
-import {AUTH_CHECK_STATE, AUTH_CHECK_TIMEOUT, AUTH_INITIATE_LOGOUT, AUTH_USER} from "../actions/actionTypes";
+import {
+  AUTH_CHECK_STATE,
+  AUTH_CHECK_TIMEOUT,
+  AUTH_INITIATE_LOGOUT,
+  AUTH_USER,
+  INIT_INGREDIENTS
+} from "../actions/actionTypes";
+import {initIngredientsSaga} from "./burgerBuilder";
 
 export function* watchAuth() {
   yield takeEvery(AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(AUTH_USER, authUserSaga);
   yield takeEvery(AUTH_CHECK_STATE, authCheckStateSaga);
+}
+
+export function* watchBurgerBuilder() {
+  yield takeEvery(INIT_INGREDIENTS, initIngredientsSaga);
 }
