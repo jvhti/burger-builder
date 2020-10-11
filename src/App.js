7b18d6abf7563aefcccad9efb4229a082b9d741b
jvhti <jvhti@hotmail.com>
2020-10-11 13:42:19 -0300

    Convert App to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
@@ -11,39 +11,37 @@ const asyncCheckout = asyncComponent(() => import('./containers/Checkout/Checkou
 const asyncOrders = asyncComponent(() => import('./containers/Orders/Orders'));
 const asyncAuth = asyncComponent(() => import('./containers/Auth/Auth'));
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignUp();
-  }
+const App = (props) => {
+  useEffect(() => {
+    props.onTryAutoSignUp();
+  }, []);
 
-  render() {
-    let routes = (
+  let routes = (
+      <Switch>
+        <Route path="/auth" component={asyncAuth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to="/"/>
+      </Switch>
+  );
+
+  if (props.isAuthenticated)
+    routes = (
         <Switch>
+          <Route path="/checkout" component={asyncCheckout}/>
+          <Route path="/orders" component={asyncOrders}/>
           <Route path="/auth" component={asyncAuth}/>
+          <Route path="/logout" component={Logout}/>
           <Route path="/" exact component={BurgerBuilder}/>
           <Redirect to="/"/>
         </Switch>
     );
 
-    if (this.props.isAuthenticated)
-      routes = (
-          <Switch>
-            <Route path="/checkout" component={asyncCheckout}/>
-            <Route path="/orders" component={asyncOrders}/>
-            <Route path="/auth" component={asyncAuth}/>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" exact component={BurgerBuilder}/>
-            <Redirect to="/"/>
-          </Switch>
-      );
-
-    return (
-        <Layout>
-          {routes}
-        </Layout>
-    );
-  }
-}
+  return (
+      <Layout>
+        {routes}
+      </Layout>
+  );
+};
 
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.token !== null
